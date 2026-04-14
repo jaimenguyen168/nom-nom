41c578dcb5ee307c478e66cd87560e37a7b73498
jaimenguyen168 <77992599+jaimenguyen168@users.noreply.github.com>
2026-04-14 10:51:55 -0400

    Added proper routing

---
+++ src/app/(public)/blogs/[blogId]/page.tsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function PublicDetailsBlogPage() {
+  return <div>PublicDetailsBlogPage</div>;
+}
---
+++ src/app/(public)/recipes/page.tsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function PublicRecipesPage() {
+  return <div>PublicRecipesPage</div>;
+}
