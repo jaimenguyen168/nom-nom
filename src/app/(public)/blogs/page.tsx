ca307bfa7854625d5b8362c378063c383059af3e
jaimenguyen168 <77992599+jaimenguyen168@users.noreply.github.com>
2026-04-16 10:59:38 -0400

    Fix build

--- src/app/(public)/blogs/page.tsx
+++ src/app/(public)/blogs/page.tsx
@@ -2,6 +2,8 @@ import React from "react";
 import BlogsView from "@/features/blogs/views/blogs-view";
 import { HydrateClient, prefetch, trpc } from "@/trpc/server";
 
+export const dynamic = "force-dynamic";
+
 export default async function BlogsPage() {
   prefetch(trpc.blogs.getMany.queryOptions({}));
 
